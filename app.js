4c3e0e74e5ad1c4663e75f194462167d5bf16544
unknown <zarghamkhan980@gmail.com>
2020-06-25 00:42:12 +0200

    pool and step logic finished

--- app.js
+++ app.js
@@ -13,18 +13,25 @@ const stepWidthSelector = document.getElementById("step-width");
 const sideStepVisibilitySelector = document.getElementById(
   "side-step-visibility"
 );
+// select side step orientation selector
+const sideStepOrientationSelector = document.getElementById(
+  "side-step-orientation"
+);
 
 // get default values from selectors
 let poolLength =
   poolLengthSelector.options[poolLengthSelector.selectedIndex].value;
 let poolWidth =
   poolWidthSelector.options[poolWidthSelector.selectedIndex].value;
-const stepLength =
+let stepLength =
   stepLengthSelector.options[stepLengthSelector.selectedIndex].value;
-const stepWidth =
+let stepWidth =
   stepWidthSelector.options[stepWidthSelector.selectedIndex].value;
 let sideStepVisibility =
   sideStepVisibilitySelector.options[sideStepVisibilitySelector.selectedIndex]
+    .value;
+let sideStepOrientation =
+  sideStepOrientationSelector.options[sideStepOrientationSelector.selectedIndex]
     .value;
 
 // get pools width in pixels from dom and find scale factor
@@ -43,6 +50,15 @@ const render = () => {
     sideStep.style.display = "block";
   }
 
+  // set sidestep orientation
+  if (sideStepOrientation === "right") {
+    sideStep.style.alignSelf = "flex-end";
+  } else if (sideStepOrientation === "left") {
+    sideStep.style.alignSelf = "flex-start";
+  } else if (sideStepOrientation === "centre") {
+    sideStep.style.alignSelf = "center";
+  }
+
   // set step length and width
   sideStep.style.width = `${stepLength * scaleFactor}`;
   sideStep.style.height = `${stepWidth * scaleFactor}`;
@@ -51,17 +67,27 @@ const render = () => {
 render();
 
 // add event listeners
-
 sideStepVisibilitySelector.addEventListener("change", (event) => {
   sideStepVisibility = event.target.value;
   render();
 });
-
 poolLengthSelector.addEventListener("change", (event) => {
   poolLength = event.target.value;
   render();
 });
 poolWidthSelector.addEventListener("change", (event) => {
   poolWidth = event.target.value;
+  render();
+});
+stepLengthSelector.addEventListener("change", (event) => {
+  stepLength = event.target.value;
+  render();
+});
+stepWidthSelector.addEventListener("change", (event) => {
+  stepWidth = event.target.value;
+  render();
+});
+sideStepOrientationSelector.addEventListener("change", (event) => {
+  sideStepOrientation = event.target.value;
   render();
 });
