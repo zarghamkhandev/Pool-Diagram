f0c4fa8ed45b5d641278c37fa6bcbc6da617ebcd
unknown <zarghamkhan980@gmail.com>
2020-06-24 23:42:07 +0200

    adding event listeners

--- app.js
+++ app.js
@@ -1,9 +1,67 @@
+// select main pool shape
 const mainPool = document.querySelector("#main-pool");
+// select step shape
+const sideStep = document.querySelector("#side-step");
+// select pool's length and width
+const poolLengthSelector = document.getElementById("pool-length");
+const poolWidthSelector = document.getElementById("pool-width");
+// select side step length
+const stepLengthSelector = document.getElementById("step-length");
+// select side step width
+const stepWidthSelector = document.getElementById("step-width");
+// select side step visibility
+const sideStepVisibilitySelector = document.getElementById(
+  "side-step-visibility"
+);
 
-const poolLength = 8.2;
-const poolWidth = 6;
+// get default values from selectors
+let poolLength =
+  poolLengthSelector.options[poolLengthSelector.selectedIndex].value;
+let poolWidth =
+  poolWidthSelector.options[poolWidthSelector.selectedIndex].value;
+const stepLength =
+  stepLengthSelector.options[stepLengthSelector.selectedIndex].value;
+const stepWidth =
+  stepWidthSelector.options[stepWidthSelector.selectedIndex].value;
+let sideStepVisibility =
+  sideStepVisibilitySelector.options[sideStepVisibilitySelector.selectedIndex]
+    .value;
 
-const poolRatio = ((poolWidth / poolLength) * 100).toFixed();
-console.log(poolRatio);
+// get pools width in pixels from dom and find scale factor
+const poolLengthInPixel = mainPool.clientWidth;
+const scaleFactor = poolLengthInPixel / poolLength;
 
-mainPool.style.paddingBottom = `${poolRatio}%`;
+const render = () => {
+  // set pool's width
+  const poolRatio = ((poolWidth / poolLength) * 100).toFixed();
+  mainPool.style.paddingBottom = `${poolRatio}%`;
+
+  // set visibility of side step
+  if (sideStepVisibility === "no") {
+    sideStep.style.display = "none";
+  } else if (sideStepVisibility === "yes") {
+    sideStep.style.display = "block";
+  }
+
+  // set step length and width
+  sideStep.style.width = `${stepLength * scaleFactor}`;
+  sideStep.style.height = `${stepWidth * scaleFactor}`;
+};
+
+render();
+
+// add event listeners
+
+sideStepVisibilitySelector.addEventListener("change", (event) => {
+  sideStepVisibility = event.target.value;
+  render();
+});
+
+poolLengthSelector.addEventListener("change", (event) => {
+  poolLength = event.target.value;
+  render();
+});
+poolWidthSelector.addEventListener("change", (event) => {
+  poolWidth = event.target.value;
+  render();
+});
